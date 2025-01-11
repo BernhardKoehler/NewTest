845d550c4678f340453e78355d77665db3fa484b
BernhardKoehler <BernhardKoehler@web.de>
2025-01-11 15:04:28 +0100

    xx

--- scraper.js
+++ scraper.js
@@ -5,8 +5,6 @@ import "sqlite3";
 import "axios";
 import "dotenv";
 
-dotenv.config();
-
 function initDatabase(callback) {
   // Set up sqlite database.
   var db = new sqlite3.Database("data.sqlite");
